7171151d94ba6460e84437ad5cf726c4c0e91770
AfanasevArsenii <afanasev.arsenii@phystech.edu>
2021-11-27 21:19:22 +0300

    Главный модуль программы.

---
+++ draw_constructor.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Dolgoprudniy Program")
+pygame.display.set_icon(pygame.image.load("emblem.ico"))
