6385a64e7fb9452dd9ff7fd54e541373b6009f85
Bionhen <liubimov.is@phystech.edu>
2021-11-28 20:02:25 +0300

    Корректировки в модулях, добавление функции перемещения в конструктор

--- starship_constructor.py
+++ starship_constructor.py
@@ -50,16 +50,34 @@ def net():
             net_list[i][j] = 1 + 10 * i + j
 
 
+def module_moving(module, main_surface):
+    """
+    Функция перемещения модуля после его выбора в левом меню
+
+    """
+    x, y = pygame.mouse_get_pos
+    sc = main_surface
+    flag1 = False
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTOMDOWN:
+            # FIXME if x, y - условие на попадание в область модуля, где мы их выбираем (левая панель)
+                 flag1 = True
+        if event.type == pygame.MOUSEBUTTOMDOWN:
+            flag1 = False
+        while flag1:
+            sc.blit(module.image, (x - module.b / 2, y - module.a / 2))
+
+
 def set_module(module, main_surface):
     """
     Функция ставит изображение модуля в левый верхний угол сетки
     Args:
-    module: -экземпляр класса модуль
+    module: экземпляр класса модуль
     main_surface: главная поверхность
     """
     x, y = pygame.mouse_get_pos
     sc = main_surface
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTOUP:
             if 100 < x < 700 and 50 < y < 650:
-                sc.blit(module.image, (x-x % 50-100, y-50-y % 25))
+                sc.blit(module.image, (x - x % 50 - 100, y - 50 - y % 25))
