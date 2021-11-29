371a594e8ba707dbb8c0d9173e1d75be9405254c
Pavel Tsarev <paveltsarev2002@yandex.ru>
2021-11-29 17:57:27 +0300

    Исправлена функция draw_menu. Изменены координаты кнопок во всех файлах. Программа main_program доработана так, что переключает слайд

--- change_screen.py
+++ change_screen.py
@@ -7,7 +7,7 @@ def show_screen(draw_screen, mouse_x, mouse_y):
     :param mouse_y: вертикальная координата точки, в которой произошел щелчок мыши
     :return: draw_screen
     """
-    if (draw_screen == "menu") and (mouse_x >= 325) and (mouse_x <= 475) and (mouse_y <= 500) and mouse_y >= 400:
+    if (draw_screen == "menu") and (mouse_x >= 325) and (mouse_x <= 475) and (mouse_y <= 450) and mouse_y >= 400:
         draw_screen = "list_of_missions"
     # в следующие 2 цикла if надо вставить реальные границы кнопок
     elif (draw_screen == "list_of_missions") and (mouse_x >= 1) and (mouse_x <= 2) and (mouse_y <= 3) and mouse_y >= 4:
