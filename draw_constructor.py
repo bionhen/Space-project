0373bc5698f10a94453f16d7e24b566a1eb57427
AfanasevArsenii <afanasev.arsenii@phystech.edu>
2021-12-01 11:20:18 +0300

    Движение модулей

--- draw_constructor.py
+++ draw_constructor.py
@@ -140,6 +140,8 @@ def draw_constructor():
     buttons_off, buttons_on = render_buttons()
     dif_module_surf_list = render_module_surf_list(blocks)
 
+    move_modules(dif_module_surf_list, bg_constructor_surf)
+
     draw_buttons(bg_constructor_surf, buttons_off, buttons_on)
 
     draw_modules(dif_module_surf_list, bg_constructor_surf)
@@ -149,6 +151,8 @@ def draw_constructor():
     draw_points()
 
     draw_rocket_surface()
+
+
 
 
 while True:
